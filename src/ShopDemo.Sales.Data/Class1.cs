2ca74e69acddb8f0aa244d8c58087c3bdca78148
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-31 18:54:36 -0300

    Starting ordem item test (Red)

--- src/ShopDemo.Sales.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ShopDemo.Sales.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ tests/ShopDemo.Sales.Domain.Tests/OrderTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace ShopDemo.Sales.Domain.Tests
+{
+    public class OrderTests
+    {
+        [Fact(DisplayName = "Add Item Order Empty")]
+        [Trait("Category", "Order Tests")]
+        public void Add_OrderItem_NewOrder_ShouldUpdateValie()
+        {
+            // Arrange
+            var order = new Order();
+            var orderItem = new OrderItem(Guid.NewGuid(),"Product Test", 2, 100);
+
+            // Act
+            order.AddItem(orderItem);
+
+            // Assert
+            Assert.Equal(200, order.TotalValue);
+        }
+    }
+}
