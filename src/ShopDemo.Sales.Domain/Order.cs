5e760730d72af9d9bd44b85490f77ff85aca9875
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-31 18:58:49 -0300

    Finishing ordem item test (Red)

---
+++ src/ShopDemo.Sales.Domain/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopDemo.Sales.Domain
+{
+    public class Order
+    {
+        public decimal TotalValue { get; set; }
+        public void AddItem(OrderItem orderItem)
+        {
+
+        }
+    }
+
+    public class OrderItem
+    {
+        public OrderItem(Guid id, string productName, int quantity, decimal unitValue)
+        {
+            Id = id;
+            ProductName = productName;
+            Quantity = quantity;
+            UnitValue = unitValue;
+        }
+
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitValue { get; set; }
+    }
+}
