1c0cb2517570a6b7c9af4df76cb75996a8c67954
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-02-03 22:26:28 -0300

    Red test on Validate Voucher type valid value

---
+++ src/ShopDemo.Sales.Domain/Voucher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopDemo.Sales.Domain
+{
+    public class Voucher
+    {
+        public string Code { get; set; }
+        public decimal? DiscountPercent { get; set; }
+        public decimal? DiscountValue { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ValidateDate { get; set; }
+        public bool Active { get; set; }
+        public bool Used { get; set; }
+
+        public bool ValidateIfApplicable()
+        {
+            return false;
+        }
+    }
+}
---
+++ tests/ShopDemo.Sales.Domain.Tests/VoucherTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+namespace ShopDemo.Sales.Domain.Tests
+{
+    public class VoucherTests
+    {
+        [Fact(DisplayName = "Validate Voucher Type Valid Value")]
+        [Trait("Category", "Sales - Voucher")]
+        public void Voucher_ValidateVoucherTypeValue_ShouldBeValid()
+        {
+            // Arrange
+            var voucher = new Voucher
+            {
+                Code = "PROMO-15-REAIS",
+                DiscountValue = 15,
+                DiscountPercent = null,
+                Quantity = 1,
+                ValidateDate = DateTime.Now,
+                Active = true,
+                Used = false
+            };
+
+            // Act
+            var result = voucher.ValidateIfApplicable();
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
