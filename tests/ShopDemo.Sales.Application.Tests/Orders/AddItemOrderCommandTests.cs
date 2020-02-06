5614e8a3465d5845a7a59c4e1f218d33dce8bce2
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-02-05 22:27:33 -0300

    Start command add max item item order test(refactor)

--- tests/ShopDemo.Sales.Application.Tests/Orders/AddItemOrderCommandTests.cs
+++ tests/ShopDemo.Sales.Application.Tests/Orders/AddItemOrderCommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using ShopDemo.Sales.Application.Commands;
+using ShopDemo.Sales.Domain;
 using System;
 using System.Linq;
 using Xunit;
@@ -8,7 +9,7 @@ namespace ShopDemo.Sales.Application.Tests.Orders
     {
         [Fact(DisplayName = "Add Item Command Valid")]
         [Trait("Category", "Sales - OrderCommands")]
-        public void AddItemOrderCommand_CommandIsValid_ShouldPassInValidation()
+        public void AddItemOrderCommand_CommandIsValid_ShouldPassOnValidation()
         {
             // Arrange
             var orderCommand = new AddItemOrderCommand(Guid.NewGuid(), Guid.NewGuid(), "Product Test", 2, 100);
@@ -22,7 +23,7 @@ namespace ShopDemo.Sales.Application.Tests.Orders
 
         [Fact(DisplayName = "Add Item Command Invalid")]
         [Trait("Category", "Sales - OrderCommands")]
-        public void AddItemOrderCommand_CommandIsInvalid_ShouldntPassInValidation()
+        public void AddItemOrderCommand_CommandIsInvalid_ShouldntPassOnValidation()
         {
             // Arrange
             var orderCommand = new AddItemOrderCommand(Guid.Empty, Guid.Empty, "", 0, 0);
@@ -37,6 +38,21 @@ namespace ShopDemo.Sales.Application.Tests.Orders
             Assert.Contains(AddOrderItemValidation.NameErrorMsg, orderCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
             Assert.Contains(AddOrderItemValidation.QtyMinErrorMsg, orderCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
             Assert.Contains(AddOrderItemValidation.ValueErrorMsg, orderCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+        }
+
+        [Fact(DisplayName = "Add Item Command unit above the allowed")]
+        [Trait("Category", "Sales - OrderCommands")]
+        public void AddItemOrderCommand_UnitQuantityAboveAllowed_ShouldntPassOnValidation()
+        {
+            // Arrange
+            var orderCommand = new AddItemOrderCommand(Guid.NewGuid(), Guid.NewGuid(), "Product Test", Order.MAX_UNIT_ITEM + 1, 100);
+
+            // Act
+            var result = orderCommand.IsValid();
+
+            // Assert
+            Assert.False(result);
+            Assert.Contains(AddOrderItemValidation.QtyMaxErrorMsg, orderCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
         }
     }
 }
