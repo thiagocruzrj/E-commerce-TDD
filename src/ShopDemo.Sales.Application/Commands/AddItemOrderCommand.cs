cab0ffef3739a7e2e0a17518717a41624aa88ee7
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-02-05 21:38:37 -0300

    Start command add item order test(red)

---
+++ src/ShopDemo.Sales.Application/Commands/AddItemOrderCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ShopDemo.Sales.Application.Commands
+{
+    public class AddItemOrderCommand
+    {
+        public AddItemOrderCommand(Guid clientId, Guid productId, string name, int quantity, decimal unitValue)
+        {
+            ClientId = clientId;
+            ProductId = productId;
+            Name = name;
+            Quantity = quantity;
+            UnitValue = unitValue;
+        }
+
+        public Guid ClientId { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitValue { get; set; }
+
+        public bool IsValid()
+        {
+            return false;
+        }
+    }
+}
---
+++ tests/ShopDemo.Sales.Application.Tests/Orders/AddItemOrderCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using ShopDemo.Sales.Application.Commands;
+using System;
+using Xunit;
+namespace ShopDemo.Sales.Application.Tests.Orders
+{
+    public class AddItemOrderCommandTests
+    {
+        [Fact(DisplayName = "Add Item Command Valid")]
+        [Trait("Category", "Sales - OrderCommands")]
+        public void AddItemOrderCommand_CommandIsValid_ShouldPassInValidation()
+        {
+            // Arrange
+            var orderCommand = new AddItemOrderCommand(Guid.NewGuid(), Guid.NewGuid(), "Product Test", 2, 100);
+
+            // Act
+            var result = orderCommand.IsValid();
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
