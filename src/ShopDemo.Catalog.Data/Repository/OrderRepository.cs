537aaf96fbe05d57a65ffad540f55a37ea79801e
Thiago Cruz <thagocruz@gmail.com>
2020-02-11 21:46:47 -0300

    Implementing OrderRepository

---
+++ src/ShopDemo.Catalog.Data/Repository/OrderRepository.cs
@@ -0,0 +1,49 @@
+﻿using ShopDemo.Core.Data;
+using ShopDemo.Sales.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Catalog.Data.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly SalesContext _context;
+
+        public OrderRepository(SalesContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public void Add(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetDraftByClientId(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(OrderItem orderItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/ShopDemo.Sales.Domain/IOrderRepository.cs
+++ src/ShopDemo.Sales.Domain/IOrderRepository.cs
@@ -1,15 +1,22 @@
 ﻿using ShopDemo.Core.Data;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ShopDemo.Sales.Domain
 {
     public interface IOrderRepository : IRepository<Order>
     {
+        Task<IEnumerable<Order>> GetListByClientId(Guid clientId);
+        Task<Order> GetOrderDraftByClientId(Guid clientId);
         void Add(Order order);
         void Update(Order order);
-        Task<Order> GetDraftByClientId(Guid clientId);
+
+        Task<OrderItem> GetItemByOrder(Guid orderId, Guid productId);
         void AddItem(OrderItem orderItem);
         void UpdateItem(OrderItem orderItem);
+        void RemoveItem(OrderItem orderItem);
+
+        Task<Voucher> GetVoucherByCode(string codigo);
     }
 }
