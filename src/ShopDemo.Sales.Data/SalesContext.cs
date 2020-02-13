2541a7ba8435089783896f4bd90a75e21a280674
Thiago Cruz <thagocruz@gmail.com>
2020-02-12 23:47:40 -0300

    Implementing Sales Context OnModel and Commit

--- src/ShopDemo.Sales.Data/SalesContext.cs
+++ src/ShopDemo.Sales.Data/SalesContext.cs
@@ -1,15 +1,62 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using ShopDemo.Core.Data;
+using ShopDemo.Core.Messages;
+using ShopDemo.Sales.Domain;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ShopDemo.Sales.Data
 {
     public class SalesContext : DbContext, IUnitOfWork
     {
-        public Task<bool> Commit()
+        private readonly IMediator _mediator;
+
+        public SalesContext(DbContextOptions<SalesContext> options, IMediator mediator)
         {
-            throw new NotImplementedException();
+            _mediator = mediator;
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Voucher> Vouchers { get; set; }
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("RegisterDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("RegisterDate").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("RegisterDate").IsModified = false;
+                }
+            }
+
+            var success = await base.SaveChangesAsync() > 0;
+            if (success) await _mediator.PublishEvents(this);
+
+            return success;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");    
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SalesContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.HasSequence<int>("MySequence").StartsAt(1000).IncrementsBy(1);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
