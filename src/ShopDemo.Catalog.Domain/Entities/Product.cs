8af2292dc506aa24aee7b800db758e0593d88656
Thiago Cruz <thagocruz@gmail.com>
2020-03-03 00:48:12 -0300

    Start implementation of Product entity

--- src/ShopDemo.Catalog.Domain/Entities/Product.cs
+++ src/ShopDemo.Catalog.Domain/Entities/Product.cs
@@ -1,6 +1,21 @@
-﻿namespace ShopDemo.Catalog.Domain.Entities
+﻿using ShopDemo.Core.DomainObjects;
+using System;
+
+namespace ShopDemo.Catalog.Domain.Entities
 {
-    public class Product
+    public class Product : Entity, IAggregateRoot
     {
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime RegisterDate { get; private set; }
+        public string Image { get; private set; }
+        public int StockQuantity { get; private set; }
+        public Dimensions Dimensions { get; private set; }
+        public Category Category { get; private set; }
+
+        protected Product() { }
     }
 }
