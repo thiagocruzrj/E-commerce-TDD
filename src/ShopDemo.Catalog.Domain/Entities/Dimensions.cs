e80bb937f5cd6dd5ec3fad8cfbeb028cf76d2d58
Thiago Cruz <thagocruz@gmail.com>
2020-03-03 00:28:41 -0300

    Remove unecessary libs

--- src/ShopDemo.Catalog.Domain/Entities/Dimensions.cs
+++ src/ShopDemo.Catalog.Domain/Entities/Dimensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ShopDemo.Catalog.Domain.Entities
+﻿namespace ShopDemo.Catalog.Domain.Entities
 {
     class Dimensions
     {
--- src/ShopDemo.Catalog.Domain/IStockService.cs
+++ src/ShopDemo.Catalog.Domain/IStockService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ShopDemo.Catalog.Domain
+﻿namespace ShopDemo.Catalog.Domain
 {
     interface IStockService
     {
