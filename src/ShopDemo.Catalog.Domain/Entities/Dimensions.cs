8dec735446a7843e026ae486f9e7a0e5058fcc4f
Thiago Cruz <thagocruz@gmail.com>
2020-03-03 00:45:06 -0300

    Implement Dimensions entity

--- src/ShopDemo.Catalog.Domain/Entities/Dimensions.cs
+++ src/ShopDemo.Catalog.Domain/Entities/Dimensions.cs
@@ -1,6 +1,26 @@
 ﻿namespace ShopDemo.Catalog.Domain.Entities
 {
-    class Dimensions
+    public class Dimensions
     {
+        public Dimensions(decimal height, decimal width, decimal depth)
+        {
+            Height = height;
+            Width = width;
+            Depth = depth;
+        }
+
+        public decimal Height { get; private set; }
+        public decimal Width { get; private set; }
+        public decimal Depth { get; private set; }
+
+        public string DescriptionFormatter()
+        {
+            return $"HxWxD: {Height} x {Width} x {Depth}";
+        }
+
+        public override string ToString()
+        {
+            return DescriptionFormatter();
+        }
     }
 }
