b6d44e44597fbda4346242b85c0efde64b8288ed
Thiago Cruz <thagocruz@gmail.com>
2020-03-03 23:18:37 -0300

    Add product repository interface

--- src/ShopDemo.Catalog.Domain/IProductRepository.cs
+++ src/ShopDemo.Catalog.Domain/IProductRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace ShopDemo.Catalog.Domain
+﻿using ShopDemo.Catalog.Domain.Entities;
+using ShopDemo.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Catalog.Domain
 {
-    interface IProductRepository
+    public interface IProductRepository : IRepository<Product>
     {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(Guid id);
+        Task<IEnumerable<Product>> GetProductByCategoryCode(int code);
+        Task<IEnumerable<Category>> GetAllCategories();
+
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
     }
 }
