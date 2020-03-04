c5579cf237180f93be0cc2800616d3c48fcb97cf
Thiago Cruz <thagocruz@gmail.com>
2020-03-03 23:20:30 -0300

    Start StockService implementation

--- src/ShopDemo.Catalog.Domain/StockService.cs
+++ src/ShopDemo.Catalog.Domain/StockService.cs
@@ -1,6 +1,33 @@
-﻿namespace ShopDemo.Catalog.Domain
+﻿using MediatR;
+using System;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Catalog.Domain
 {
-    class StockService
+    public class StockService : IStockService
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IMediator _mediator;
+
+        public StockService(IProductRepository productRepository, IMediator mediator)
+        {
+            _productRepository = productRepository;
+            _mediator = mediator;
+        }
+
+        public Task<bool> ReplanishItemOnStock(Guid productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveItemFromStock(Guid productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
