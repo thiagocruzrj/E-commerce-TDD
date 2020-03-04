7adc4bd56f7e5bb2912f55fe78435c1b57ee6d56
Thiago Cruz <thagocruz@gmail.com>
2020-03-03 23:13:04 -0300

    Finish entity product implementation

--- src/ShopDemo.Catalog.Domain/Entities/Product.cs
+++ src/ShopDemo.Catalog.Domain/Entities/Product.cs
@@ -17,5 +17,54 @@ namespace ShopDemo.Catalog.Domain.Entities
         public Category Category { get; private set; }
 
         protected Product() { }
+
+        public Product(Guid categoryId, 
+            string name, string description, 
+            bool active, decimal value, 
+            DateTime registerDate, string image, 
+            int stockQuantity, Dimensions dimensions)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Value = value;
+            RegisterDate = registerDate;
+            Image = image;
+            StockQuantity = stockQuantity;
+            Dimensions = dimensions;
+        }
+
+        public void Ative() => Active = true;
+
+        public void Deactive() => Active = false;
+
+        public void UpdateCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void UpdateDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void RemoveStockItem(int quantity)
+        {
+            if (quantity < 0) quantity *= -1;
+            if (!HasOnStock(quantity)) throw new DomainException("Insuficient stock");
+            StockQuantity -= quantity;
+        }
+
+        public void ReplenishStock(int quantity)
+        {
+            StockQuantity += quantity;
+        }
+
+        public bool HasOnStock(int quantity)
+        {
+            return StockQuantity >= quantity;
+        }
     }
 }
