6635594d10991c41bf37a4e4f1f89dc16d1d49e6
Thiago Cruz <thagocruz@gmail.com>
2020-03-05 00:23:22 -0300

    Start Implementing ProductRepository

---
+++ src/ShopDemo.Catalog.Data/Repository/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using ShopDemo.Catalog.Domain;
+using ShopDemo.Catalog.Domain.Entities;
+using ShopDemo.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Catalog.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly CatalogContext _context;
+
+        public ProductRepository(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public Task<IEnumerable<Category>> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategoryCode(int code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
