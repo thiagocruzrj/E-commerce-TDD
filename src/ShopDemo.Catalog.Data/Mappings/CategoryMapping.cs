4f9820c451b1a50f0f7186daeb9aa174d4669b40
Thiago Cruz <thagocruz@gmail.com>
2020-03-05 00:03:15 -0300

    Implement category mapping

--- src/ShopDemo.Catalog.Data/Mappings/CategoryMapping.cs
+++ src/ShopDemo.Catalog.Data/Mappings/CategoryMapping.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopDemo.Catalog.Domain.Entities;
 
 namespace ShopDemo.Catalog.Data.Mappings
 {
-    class CategoryMapping
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            // 1 : N Category : Products
+            builder.HasMany(p => p.Products)
+                .WithOne(c => c.Category)
+                .HasForeignKey(p => p.CategoryId);
+
+            builder.ToTable("Categories");
+        }
     }
 }
