2cb8aa479032953d8d4582e435f397b4968887a5
Thiago Cruz <thagocruz@gmail.com>
2020-03-04 23:50:09 -0300

    Start Implementing CatalogContext class

---
+++ src/ShopDemo.Catalog.Data/CatalogContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopDemo.Catalog.Domain.Entities;
+using ShopDemo.Core.Data;
+using ShopDemo.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Catalog.Data
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
