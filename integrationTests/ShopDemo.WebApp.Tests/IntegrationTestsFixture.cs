a58c95955dcbdb06652c8dec8b20e5d79ecf801a
Thiago Cruz <thagocruz@gmail.com>
2020-03-30 19:45:07 -0300

    Create Integration Fixture

---
+++ integrationTests/ShopDemo.WebApp.Tests/IntegrationTestsFixture.cs
@@ -0,0 +1,30 @@
+﻿using ShopDemo.WebApp.MVC;
+using ShopDemo.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace ShopDemo.WebApp.Tests
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
+
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly ShopAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture(ShopAppFactory<TStartup> factory, HttpClient client)
+        {
+            Factory = new ShopAppFactory<TStartup>();
+            Client = Factory.CreateClient();
+        }
+
+        public void Dispose() { }
+    }
+}
