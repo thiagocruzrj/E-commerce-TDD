7d4c7ca0f8e6f3f7c7ed2e7510ab5b05f05a16ee
Thiago Cruz <thagocruz@gmail.com>
2020-05-28 12:31:37 -0300

    Add commom steps

---
+++ 04 - BDD/NerdStore.BDD.Tests/Usuario/CommomSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace NerdStore.BDD.Tests.Usuario
+{
+    [Binding]
+    public class CommomSteps
+    {
+        [Then(@"Uma saudação com seu e-mail será exibida no menu superior")]
+        public void EntaoUmaSaudacaoComSeuE_MailSeraExibidaNoMenuSuperior()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"Que o visitatnte está acessando o site da loja")]
+        public void DadoQueOVisitatnteEstaAcessandoOSiteDaLoja()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Ele será redirecionado na vitrine")]
+        public void EntaoEleSeraRedirecionadoNaVitrine()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
--- 04 - BDD/NerdStore.BDD.Tests/Usuario/Usuario_CadastroSteps.cs
+++ 04 - BDD/NerdStore.BDD.Tests/Usuario/Usuario_CadastroSteps.cs
@@ -5,12 +5,6 @@ namespace NerdStore.BDD.Tests.Usuario
     [Binding]
     public class Usuario_CadastroSteps
     {
-        [Given(@"Que o visitatnte está acessando o site da loja")]
-        public void DadoQueOVisitatnteEstaAcessandoOSiteDaLoja()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
         [When(@"Ele clicar em registrar")]
         public void QuandoEleClicarEmRegistrar()
         {
@@ -40,19 +34,7 @@ namespace NerdStore.BDD.Tests.Usuario
         {
             ScenarioContext.Current.Pending();
         }
-        
-        [Then(@"Ele será redirecionado na vitrine")]
-        public void EntaoEleSeraRedirecionadoNaVitrine()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"Uma saudação com seu e-mail será exibida no menu superior")]
-        public void EntaoUmaSaudacaoComSeuE_MailSeraExibidaNoMenuSuperior()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
+
         [Then(@"Ele receberá uma mensagem de error que a senha precisa conter uma letra maiuscula")]
         public void EntaoEleReceberaUmaMensagemDeErrorQueASenhaPrecisaConterUmaLetraMaiuscula()
         {
